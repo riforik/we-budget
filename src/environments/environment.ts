714ac4e0062b4aab186a166d39f3d814ae50e2dd
riforik <isaiah.robinson1029@gmail.com>
2025-11-14 17:18:41 -0500

    Fix env config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   auth0: {
-    domain: "dev-deo6igg7w28l432i.us.auth0.com",
-    clientId: "rg7thY7kruB8ERzXZX7l9A4RyJ7m78Tk",
-    redirectUri: window.location.origin // http://localhost:4200
-  }
+    domain: import.meta.env.NG_APP_AUTH_DOMAIN,
+    clientId: import.meta.env.NG_APP_AUTH_CLIENT_ID,
+    redirectUri: window.location.origin, // http://localhost:4200
+  },
 };
