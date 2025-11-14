78397ba4b9788b10241fe443341ceba09355b5f9
Isaiah <isaiah.robinson1029@gmail.com>
2025-11-14 17:13:44 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   auth0: {
-    domain: import.meta.env.NG_APP_AUTH0_DOMAIN,
-    clientId: import.meta.env.NG_APP_AUTH0_CLIENT_ID,
+    domain: import.meta.env.NG_APP_AUTH_DOMAIN,
+    clientId: import.meta.env.NG_APP_AUTH_CLIENTID,
     redirectUri: window.location.origin // http://localhost:4200
   }
 };
